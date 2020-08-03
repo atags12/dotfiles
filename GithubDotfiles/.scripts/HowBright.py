936ba651d17fba7b11ddd49ff380fafdf5eab8ee
atags12 <abtag007@gmail.com>
2020-08-03 12:04:43 -0400

    removed the actual directory the files are in, now it is just my dotfiles

--- GithubDotfiles/.scripts/HowBright.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-import subprocess
-brightness = (subprocess.check_output(["xbacklight", "-get"])).decode('ascii')
-print(" " + str(round(float(brightness))) + "%")
--- GithubDotfiles/.scripts/i3cmds/toggle-touchpad.sh
+++
@@ -1,2 +0,0 @@
-#requires xorg-xinput
-if [ $(cat /tmp/touchpad_enable) == 1 ]; then echo 0 > /tmp/touchpad_enable; else echo 1 > /tmp/touchpad_enable; fi; xinput set-prop "Synaptics TM3075-002" "Device Enabled" $(cat /tmp/touchpad_enable)
