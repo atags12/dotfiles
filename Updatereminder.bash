ebb4481549f93f9bc4374b4aa4f8513200519560
atags12 <abtag007@gmail.com>
2020-09-05 10:31:59 -0400

    modified the .bashrc and Updatereminder.bash to work for any user rather than just one name

--- Updatereminder.bash
+++ Updatereminder.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -f /home/earth/.scripts/dates/`TZ=UTC+8 date +"%Y%m%d"` ]
+if [ -f $HOME/.scripts/dates/`TZ=UTC+8 date +"%Y%m%d"` ]
 then
 	echo 0 2&>/dev/null
 else
-	touch /home/earth/.scripts/dates/`TZ=UTC+8 date +"%Y%m%d" `
+	touch $HOME/.scripts/dates/`TZ=UTC+8 date +"%Y%m%d" `
 	#echo -e "$(tput setaf 1)$(tput setab 0)<SYSTEM>:"
 	echo "<SYSTEM>:"
 	echo "please run pacman -Syu"
